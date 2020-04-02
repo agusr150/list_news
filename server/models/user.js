9614442e2be3be1fc603dd17fda13617d62774b2
mohhaidir <khoiruljmbr@gmail.com>
2020-04-02 18:23:07 +0700

    create .env edit class model, google controller, google sign in

--- server/models/user.js
+++ server/models/user.js
@@ -2,17 +2,21 @@
 
 let bcrypt = require('bcrypt')
 module.exports = (sequelize, DataTypes) => {
-  const Sequelize = sequelize.Sequelize
-  const Model = Sequelize.Model
-  class User extends Model{}
+  const { Model } = sequelize.Sequelize
+
+  class User extends Model { }
   User.init({
     email: {
       type: Sequelize.STRING,
-      validate: {notEmpty: true}
-    }, 
+      validate: {
+        notEmpty: true
+      }
+    },
     password: {
       type: Sequelize.STRING,
-      validate: {notEmpty: true}
+      validate: {
+        notEmpty: true
+      }
     }
   }, {
     hooks: {
@@ -22,22 +26,11 @@ module.exports = (sequelize, DataTypes) => {
         instance.password = hash
       }
     },
-    sequelize})
-  User.associate = function(models) {
+    sequelize
+  })
+  User.associate = function (models) {
     // associations can be defined here
-    User.hasMany(models.New, {foreignKey: 'UserId'})
+    User.hasMany(models.New, { foreignKey: 'UserId' })
   };
   return User;
 }
-
-
-// module.exports = (sequelize, DataTypes) => {
-//   const User = sequelize.define('User', {
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {});
-//   User.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return User;
-// };
